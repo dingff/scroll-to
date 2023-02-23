15dca9c6d32cae3fc1da993656e978244a99b2b0
dingff <dingff0923@foxmail.com>
2023-02-23 12:04:25 +0800

    fix(background): add onUpdated listener

--- src/assets/js/background.js
+++ src/assets/js/background.js
@@ -1,9 +1,25 @@
 const SCROLL_TO_MAP = 'SCROLL_TO_MAP'
-chrome.tabs.onActivated.addListener((e) => {
-  chrome.tabs.get(e.tabId, (tab) => {
+const debounce = (fn, delay = 500) => {
+  let timer
+  return (...args) => {
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+      fn(...args)
+    }, delay)
+  }
+}
+const changeIconByTabId = (tabId) => {
+  chrome.tabs.get(tabId, (tab) => {
     chrome.storage.sync.get([SCROLL_TO_MAP]).then((res) => {
       const dataMap = res[SCROLL_TO_MAP]
       chrome.action.setIcon({ path: dataMap[tab.url] ? '../imgs/logo.png': '../imgs/logo_gray.png' })
     })
   })
+}
+chrome.tabs.onActivated.addListener((e) => {
+  changeIconByTabId(e.tabId)
 })
+chrome.tabs.onUpdated.addListener(debounce((tabId) => {
+  console.log('onUpdated', tabId);
+  changeIconByTabId(tabId)
+}))
